79cd68c5b928ba7e44581d2a10203db596b77308
theanuragshukla <tachyonanurag@gmail.com>
2023-02-04 14:58:53 +0530

    .

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box, Stack, Avatar, Typography, Link } from "@mui/material";
 import Btn from "../Components/Btn";
-import Circle from "../Components/Circle"
+import Circle from "../Components/Circle";
 
 const Home = () => {
   return (
@@ -15,7 +15,6 @@ const Home = () => {
           width: "100%",
           height: "8vh",
           bgcolor: "#ACACAC",
-
         }}
       >
         <Stack
@@ -26,7 +25,7 @@ const Home = () => {
             alignItems: "center",
           }}
         >
-          <Typography variant="h5" fontWeight={800} zIndex={2} >
+          <Typography variant="h5" fontWeight={800} zIndex={2}>
             HostelHQ
           </Typography>
           <Link to="/signup">
@@ -45,24 +44,20 @@ const Home = () => {
         width="100vw"
         overflow="hidden"
         className="body"
-        position = "relative"
+        position="relative"
       >
         <Typography variant="h1">HostelHQ</Typography>
         <Typography variant="h4" mb={2} color="#EB7C3E" marginRight="20px">
           The complete hostel management system.
         </Typography>
 
-        <Link to="/login" >
-          <Btn value="Login"
-           />
+        <Link to="/login">
+          <Btn value="Login" />
         </Link>
-      <Circle x={-100} y={-65} r={130} />
-      <Circle a={-150} b={-200} r={250} />
-      <Circle a={350} b={200} r={10} />
-
+        <Circle x={-100} y={-65} r={130} />
+        <Circle a={-150} b={-200} r={250} />
+        <Circle a={350} b={200} r={10} />
       </Box>
-
-          
     </div>
   );
 };
